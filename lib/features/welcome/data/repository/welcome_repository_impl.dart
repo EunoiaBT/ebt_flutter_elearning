3c6c6bbd3a55d31237255dc3c57c9eb0b1f764cf
kinleyrabgay <fsd.rabgay@gmail.com>
2024-09-12 20:24:27 +0600

    repository implemenation
    Signed-off-by: kinleyrabgay <fsd.rabgay@gmail.com>

---
+++ lib/features/welcome/data/repository/welcome_repository_impl.dart
@@ -0,0 +1,16 @@
+import 'package:ebt_flutter_elearning/core/utils/typedef.dart';
+import 'package:ebt_flutter_elearning/features/welcome/domain/repository/welcome_repository.dart';
+
+class WelcomeRepositoryImpl implements WelcomeRepository {
+  @override
+  ResultFuture<void> cacheFirstTimer() {
+    // TODO: implement cacheFirstTimer
+    throw UnimplementedError();
+  }
+
+  @override
+  ResultFuture<bool> checkIfUserIsFirstTimer() {
+    // TODO: implement checkIfUserIsFirstTimer
+    throw UnimplementedError();
+  }
+}
