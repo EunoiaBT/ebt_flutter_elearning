1edfa17ba7811a111ff5ddec9b6c55debf1fa6c2
kinleyrabgay <fsd.rabgay@gmail.com>
2024-09-12 19:31:51 +0600

    core:theme switch theme
    Signed-off-by: kinleyrabgay <fsd.rabgay@gmail.com>

---
+++ lib/core/themes/custom/switch_theme.dart
@@ -0,0 +1,40 @@
+import 'package:ebt_flutter_elearning/core/constants/ebt_color.dart';
+import 'package:flutter/material.dart';
+
+class GfxSwitchTheme {
+  GfxSwitchTheme._();
+
+  static SwitchThemeData lightSwitchTheme = SwitchThemeData(
+    thumbColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return EbtColor.white;
+      } else {
+        return EbtColor.primary;
+      }
+    }),
+    trackColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return EbtColor.primary;
+      } else {
+        return Colors.grey.shade300;
+      }
+    }),
+  );
+
+  static SwitchThemeData darkSwitchTheme = SwitchThemeData(
+    thumbColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.white;
+      } else {
+        return Colors.black;
+      }
+    }),
+    trackColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.blue;
+      } else {
+        return Colors.grey.shade700;
+      }
+    }),
+  );
+}
