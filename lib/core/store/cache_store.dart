39105f0e79345ec61015301f9cc70b206547bfe8
kinleyrabgay <fsd.rabgay@gmail.com>
2024-09-12 20:38:34 +0600

    added cache clear function
    Signed-off-by: kinleyrabgay <fsd.rabgay@gmail.com>

--- lib/core/store/cache_store.dart
+++ lib/core/store/cache_store.dart
@@ -1,3 +1,4 @@
+import 'package:ebt_flutter_elearning/core/constants/ebt_cache_store_key.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class CacheStore {
@@ -6,10 +7,14 @@ class CacheStore {
   const CacheStore(this._pref);
 
   Future<void> cacheFirstTimer() async {
-    await _pref.setBool('isFirstTimer', true);
+    await _pref.setBool(EbtCacheStoreKey.isFirstTimer, true);
+  }
+
+  Future<void> clearFirstTimer() async {
+    await _pref.remove(EbtCacheStoreKey.isFirstTimer);
   }
 
   Future<bool> checkIfUserIsFirstTimer() async {
-    return _pref.getBool('isFirstTimer') ?? false;
+    return _pref.getBool(EbtCacheStoreKey.isFirstTimer) ?? false;
   }
 }
