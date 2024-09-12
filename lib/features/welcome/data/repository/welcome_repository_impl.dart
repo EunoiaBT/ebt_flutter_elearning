0a7604fe72baed8fb7bfb59474703dea821ef5b2
kinleyrabgay <fsd.rabgay@gmail.com>
2024-09-12 20:27:03 +0600

    repo to source for cacheFirstTimer
    Signed-off-by: kinleyrabgay <fsd.rabgay@gmail.com>

--- lib/features/welcome/data/repository/welcome_repository_impl.dart
+++ lib/features/welcome/data/repository/welcome_repository_impl.dart
@@ -1,11 +1,25 @@
+import 'package:dartz/dartz.dart';
+import 'package:ebt_flutter_elearning/core/errors/exception.dart';
+import 'package:ebt_flutter_elearning/core/errors/failure.dart';
 import 'package:ebt_flutter_elearning/core/utils/typedef.dart';
+import 'package:ebt_flutter_elearning/features/welcome/data/datasource/local.dart';
 import 'package:ebt_flutter_elearning/features/welcome/domain/repository/welcome_repository.dart';
 
 class WelcomeRepositoryImpl implements WelcomeRepository {
+  final LocalDataSource _localDataSource;
+
+  const WelcomeRepositoryImpl(this._localDataSource);
+
   @override
-  ResultFuture<void> cacheFirstTimer() {
-    // TODO: implement cacheFirstTimer
-    throw UnimplementedError();
+  ResultFuture<void> cacheFirstTimer() async {
+    try {
+      await _localDataSource.cacheFirstTimer();
+      return const Right(null);
+    } on CacheException catch (e) {
+      return Left(
+        CacheFailure(message: e.message, statusCode: e.statusCode),
+      );
+    }
   }
 
   @override
