41fac54b79f71a0764b506d77db5d1b2c8975fd8
kinleyrabgay <fsd.rabgay@gmail.com>
2024-09-12 20:47:38 +0600

    welcome state
    Signed-off-by: kinleyrabgay <fsd.rabgay@gmail.com>

--- lib/features/welcome/presentation/cubit/welcome_state.dart
+++ lib/features/welcome/presentation/cubit/welcome_state.dart
@@ -1,6 +1,21 @@
 part of 'welcome_cubit.dart';
 
 @immutable
-sealed class WelcomeState {}
+abstract class WelcomeState extends Equatable {
+  const WelcomeState();
+
+  @override
+  List<Object?> get props => [];
+}
 
 final class WelcomeInitial extends WelcomeState {}
+
+// CacheFirstTimer
+class CacheFirstTimer extends WelcomeState {
+  const CacheFirstTimer();
+}
+
+// Check FirstTimer
+class CheckIfUserIsFirstTimer extends WelcomeState {
+  const CheckIfUserIsFirstTimer();
+}
