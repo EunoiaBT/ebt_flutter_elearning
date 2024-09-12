3d2e1b1e41ed66395051dcd518db339defb40430
kinleyrabgay <fsd.rabgay@gmail.com>
2024-09-12 19:28:52 +0600

    core:theme checkbox_theme
    Signed-off-by: kinleyrabgay <fsd.rabgay@gmail.com>

---
+++ lib/core/themes/custom/checkbox_theme.dart
@@ -0,0 +1,42 @@
+import 'package:ebt_flutter_elearning/core/constants/ebt_color.dart';
+import 'package:flutter/material.dart';
+
+class EbtCheckboxTheme {
+  EbtCheckboxTheme._();
+
+  static CheckboxThemeData lightCheckboxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return EbtColor.white;
+      } else {
+        return EbtColor.primary;
+      }
+    }),
+    fillColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return EbtColor.primary;
+      } else {
+        return Colors.transparent;
+      }
+    }),
+  );
+
+  static CheckboxThemeData darkCheckboxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.white;
+      } else {
+        return Colors.black;
+      }
+    }),
+    fillColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.blue;
+      } else {
+        return Colors.transparent;
+      }
+    }),
+  );
+}
