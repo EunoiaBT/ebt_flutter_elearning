a1e82449e72c7a050a05f557aecca1b4498527d0
kinleyrabgay <fsd.rabgay@gmail.com>
2024-09-12 19:55:57 +0600

    service injection for utlity
    Signed-off-by: kinleyrabgay <fsd.rabgay@gmail.com>

--- lib/config/injection/injection.dart
+++ lib/config/injection/injection.dart
@@ -1,1 +1,6 @@
-part of 'injection.main.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+part 'injection.main.dart';
