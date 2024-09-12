3b1b1c364c3bc78fe3466356bec3ec1c2ad9d961
kinleyrabgay <fsd.rabgay@gmail.com>
2024-09-12 20:36:25 +0600

    cache store first timer and check
    Signed-off-by: kinleyrabgay <fsd.rabgay@gmail.com>

--- lib/core/store/cache_store.dart
+++ lib/core/store/cache_store.dart
@@ -1,3 +1,15 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 class CacheStore {
-  const CacheStore();
+  final SharedPreferences _pref;
+
+  const CacheStore(this._pref);
+
+  Future<void> cacheFirstTimer() async {
+    await _pref.setBool('isFirstTimer', true);
+  }
+
+  Future<bool> checkIfUserIsFirstTimer() async {
+    return _pref.getBool('isFirstTimer') ?? false;
+  }
 }
