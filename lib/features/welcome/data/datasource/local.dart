912d09396e594708a3b5145c95b9c898ca839a05
kinleyrabgay <fsd.rabgay@gmail.com>
2024-09-12 20:44:18 +0600

    cacheFirstTimer local source
    Signed-off-by: kinleyrabgay <fsd.rabgay@gmail.com>

--- lib/features/welcome/data/datasource/local.dart
+++ lib/features/welcome/data/datasource/local.dart
@@ -1,3 +1,7 @@
+import 'package:ebt_flutter_elearning/config/injection/injection.dart';
+import 'package:ebt_flutter_elearning/core/errors/exception.dart';
+import 'package:ebt_flutter_elearning/core/store/cache_store.dart';
+
 abstract class LocalDataSource {
   const LocalDataSource();
 
@@ -6,10 +10,15 @@ abstract class LocalDataSource {
 }
 
 class LocalDataSourceImpl implements LocalDataSource {
+  const LocalDataSourceImpl();
+
   @override
-  Future<void> cacheFirstTimer() {
-    // TODO: implement cacheFirstTimer
-    throw UnimplementedError();
+  Future<void> cacheFirstTimer() async {
+    try {
+      await sl<CacheStore>().cacheFirstTimer();
+    } catch (e) {
+      throw CacheException(message: e.toString());
+    }
   }
 
   @override
