cb401335f0b9fbd0fb405ebf46eb07b7a05e2fa3
kinleyrabgay <fsd.rabgay@gmail.com>
2024-09-12 20:22:17 +0600

    cache first timer usecase
    Signed-off-by: kinleyrabgay <fsd.rabgay@gmail.com>

--- lib/features/welcome/domain/usecase/cache_first_timer.dart
+++ lib/features/welcome/domain/usecase/cache_first_timer.dart
@@ -1,7 +1,12 @@
+import 'package:ebt_flutter_elearning/core/usecase/usecase.dart';
+import 'package:ebt_flutter_elearning/core/utils/typedef.dart';
 import 'package:ebt_flutter_elearning/features/welcome/domain/repository/welcome_repository.dart';
 
-class CacheFirstTimer {
+class CacheFirstTimer extends UsecaseWithoutParam<void> {
   final WelcomeRepository _welcomeRepository;
 
   const CacheFirstTimer(this._welcomeRepository);
+
+  @override
+  ResultFuture call() async => _welcomeRepository.cacheFirstTimer();
 }
